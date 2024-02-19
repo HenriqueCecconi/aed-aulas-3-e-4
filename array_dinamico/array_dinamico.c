6397f8f9c471ba5b6229985e154e255a0a31d911
Henrique Cecconi <henrique.cecconi@gmail.com>
2024-02-19 11:23:37 -0300

    wip aula 3 e 4

---
+++ array_dinamico/array_dinamico.c
@@ -0,0 +1,59 @@
+#include "array_dinamico.h"
+#include <stdio.h>
+
+#define INITIAL_CAPACITY 10
+
+struct ArrayDinamico
+{
+    Element* data;
+    int size; //Quantos elementos atualmente o array esta armazenando
+    int capacity; //Quantos elementos atualmente o array pode armazenar
+};
+
+ArrayDinamico* array_dinamico_create(){
+    ArrayDinamico* array = malloc(sizeof(ArrayDinamico));
+    array->capacity = INITIAL_CAPACITY;
+    array->data = malloc(sizeof(Element) * array->capacity);
+    array->size = 0;
+    return array;
+}
+
+void array_dinamico_destroy(ArrayDinamico* array){
+    free(array->data);
+    free(array);
+}
+
+void array_dinamico_print(ArrayDinamico* array){
+  for (int i = 0; i < array->size; i++) {
+    print_element(array->data[i]);
+  }
+  printf("\n");
+};
+
+Element array_dinamico_get(ArrayDinamico* array, int index){
+    if (index < 0 || index >= array->size){
+        abort();
+    }
+    return array->data[index];
+}
+
+void array_dinamico_set(ArrayDinamico* array, int index, Element value);
+
+int array_dinamico_size(ArrayDinamico* array){
+    return array->size;
+}
+
+void array_dinamico_resize(ArrayDinamico* array, int new_capacity){
+    int capacity = sizeof(Element) * new_capacity;
+    Element* new_array = realloc(array->data, capacity);
+    if(new_array != NULL){
+        array->data = new_array;
+        array->capacity = new_capacity;
+    }
+}
+
+void array_dinamico_add(ArrayDinamico* array, Element element){
+    if(array->size == array->capacity){
+
+    }
+}
